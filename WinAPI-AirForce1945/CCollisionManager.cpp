073882f776ffd4854192bd2085af25e3a9c37e1b
SoExDobin <56966606+SoexDobin@users.noreply.github.com>
2025-08-30 15:39:47 +0900

    fix : collision check at LateUpdate & add CTCCol

--- WinAPI-AirForce1945/CCollisionManager.cpp
+++ WinAPI-AirForce1945/CCollisionManager.cpp
@@ -1,2 +1,49 @@
 #include "pch.h"
 #include "CCollisionManager.h"
+
+void CCollisionManager::RTRCollision(list<CObject*>& _Src1, list<CObject*>& _Src2)
+{
+	RECT tDst{};
+	for (auto& Src1 : _Src1)
+		for (auto& Src2 : _Src2)
+		{
+			if (IntersectRect(&tDst, &(Src1->GetRect()), &(Src2->GetRect())))
+			{
+				Src1->OnCollision(Src2);
+				Src2->OnCollision(Src1);
+				Src1->SetCollision(true);
+				Src2->SetCollision(true);
+			}
+			else
+			{
+				Src1->SetCollision(false);
+				Src2->SetCollision(false);
+			}
+		}
+}
+
+void CTCCollision(list<CObject*>& _Src1, list<CObject*>& _Src2)
+{
+	for (auto& Src1 : _Src1)
+		for (auto& Src2 : _Src2)
+		{
+			Vector2 vDst = { fabsf(Src2->GetPivot().x - Src1->GetPivot().x)
+							, fabsf(Src2->GetPivot().y - Src1->GetPivot().y) };
+
+			float fDiagonal = sqrtf(vDst.GetSquared());
+			float fSumRadius = (Src2->GetSize().x + Src1->GetSize().x) * 0.5f;
+
+			if (fSumRadius >= fDiagonal)
+			{
+				Src1->OnCollision(Src2);
+				Src2->OnCollision(Src1);
+				Src1->SetCollision(true);
+				Src2->SetCollision(true);
+			}
+			else
+			{
+				Src1->SetCollision(false);
+				Src2->SetCollision(false);
+			}
+		}
+}
--- WinAPI-AirForce1945/CMainGame.cpp
+++ WinAPI-AirForce1945/CMainGame.cpp
@@ -16,7 +16,6 @@ CMainGame::~CMainGame()
 void CMainGame::Initialize()
 {
 	m_hDC = GetDC(g_hWnd);
-	
 }
 
 void CMainGame::Update()
@@ -44,26 +43,20 @@ void CMainGame::LateUpdate()
 	for (auto& list : m_ObjectList)
 		for (auto& obj : list)	
 			obj->LateUpdate();
+
+	// TODO : 충돌 처리 추가 여기서 해주세요
+	//CCollisionManager::Collision(m_ObjectList[PLAYER], m_ObjectList[ITEM], RECT_TO_RECT);
 }
 
 void CMainGame::Render()
 {
 	for (auto& list : m_ObjectList)
 		for (auto& obj : list)
-			obj->Render(m_hDC);
-
-	CCollisionManager::Collision(m_ObjectList[PLAYER], m_ObjectList[ITEM], RECT_TO_RECT);
+			obj->Render(m_hDC);	
 }
 
 void CMainGame::Release()
 {
 	for (int i = 0; i < OBJ_END; ++i)
-		for_each(m_ObjectList[i].begin(), m_ObjectList[i].end()
-			, [](CObject* _p) -> void {
-				if (_p)
-				{
-					delete _p;
-					_p = nullptr;
-				}
-			});
+		for_each(m_ObjectList[i].begin(), m_ObjectList[i].end(), SafeDelete<CObject*>);
 }
