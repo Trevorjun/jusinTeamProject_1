2558c6e98d5625889c49c855ea28fa9e12413b45
Trevorjun <150532881+Trevorjun@users.noreply.github.com>
2025-09-01 21:03:57 +0900

    chore :  monster rand 및 패턴 개선

--- WinAPI-AirForce1945/CMonster_Boss.cpp
+++ WinAPI-AirForce1945/CMonster_Boss.cpp
@@ -1,5 +1,6 @@
 ﻿#include "pch.h"
 #include "CMonster_Boss.h"
+#include <random>
 
 CMonster_Boss::CMonster_Boss()
 {
@@ -36,12 +37,18 @@ int			CMonster_Boss::Update()
 void		CMonster_Boss::LateUpdate()
 {
 	ULONGLONG dwCurrentTime = GetTickCount64();
-	srand(unsigned(GetTickCount64()));
-	int sRandNum = rand() % 3;
+	random_device rd;
+	mt19937 gen(rd());
+	vector<int> iRandNum(3, 0);
+	for (int i = 0; i < 3; i++)
+	{
+		iRandNum[i] = i;
+	}
+	shuffle(iRandNum.begin(), iRandNum.end(), gen);
 
 	if (dwCurrentTime - ull_wLastShotTime >= ULL_WSHOTINTERVAL)
 	{
-		switch (sRandNum)
+		switch (iRandNum.front())
 		{
 		case 0:
 			PatternOne();
@@ -49,9 +56,9 @@ void		CMonster_Boss::LateUpdate()
 		case 1:
 			PatternTwo();
 			break;
-		case 2:
-			PatternThree();
-			break;
+		//case 2:
+		//	PatternThree();
+		//	break;
 		default:
 			break;
 		}
@@ -73,21 +80,24 @@ void		CMonster_Boss::Release()
 
 void CMonster_Boss::PatternOne()
 {
-	for(float fBossDegree = 230.f; fBossDegree >= 310.f; fBossDegree += 20.f)
-	{
-		m_pBullet->push_back(CAbstractFactory<CNormalBullet>::Create(m_vPivot.x, m_vPivot.y + m_vSize.y / 2, OBJECT_TYPE::MONSTER_BULLET, 5.f, fBossDegree));
-	}
+	m_pBullet->push_back(CAbstractFactory<CNormalBullet>::Create(m_vPivot.x, m_vPivot.y + m_vSize.y / 2, OBJECT_TYPE::MONSTER_BULLET, 5.f, 230.f));
+	m_pBullet->push_back(CAbstractFactory<CNormalBullet>::Create(m_vPivot.x, m_vPivot.y + m_vSize.y / 2, OBJECT_TYPE::MONSTER_BULLET, 5.f, 250.f));
+	m_pBullet->push_back(CAbstractFactory<CNormalBullet>::Create(m_vPivot.x, m_vPivot.y + m_vSize.y / 2, OBJECT_TYPE::MONSTER_BULLET, 5.f, 270.f));
+	m_pBullet->push_back(CAbstractFactory<CNormalBullet>::Create(m_vPivot.x, m_vPivot.y + m_vSize.y / 2, OBJECT_TYPE::MONSTER_BULLET, 5.f, 290.f));
+	m_pBullet->push_back(CAbstractFactory<CNormalBullet>::Create(m_vPivot.x, m_vPivot.y + m_vSize.y / 2, OBJECT_TYPE::MONSTER_BULLET, 5.f, 310.f));
 }
 void CMonster_Boss::PatternTwo()
 {
 	float fBossDegree = 0.f;
 	while (fBossDegree < 360.f)
 	{
-		m_pBullet->push_back(CAbstractFactory<CNormalBullet>::Create(m_vPivot.x, m_vPivot.y, OBJECT_TYPE::MONSTER_BULLET, 5.f, fBossDegree));
+		m_pBullet->push_back(CAbstractFactory<CNormalBullet>::Create(m_vPivot.x, m_vPivot.y, OBJECT_TYPE::MONSTER_BULLET, 3.f, fBossDegree));
 		fBossDegree += 30.f;
 	}
 }
 void CMonster_Boss::PatternThree()
 {
-
+	m_pBullet->push_back(CAbstractFactory<CNormalBullet>::Create(m_vPivot.x, m_vPivot.y + m_vSize.y / 2, OBJECT_TYPE::MONSTER_BULLET, 4.f, 
+		(acos((fPlayerVX - this->GetPivot().x) / sqrtf((fPlayerVX - this->GetPivot().x) * (fPlayerVX - this->GetPivot().x) + (fPlayerVY - this->GetPivot().y) * (fPlayerVY - this->GetPivot().y))) * PI / 180.f)));
+	m_pBullet->push_back(CAbstractFactory<CNormalBullet>::Create(m_vPivot.x, m_vPivot.y + m_vSize.y / 2, OBJECT_TYPE::MONSTER_BULLET, 4.f, 290.f));
 }
