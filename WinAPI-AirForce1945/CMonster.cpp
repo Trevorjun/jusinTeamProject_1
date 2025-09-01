65e9912ccfb42906ac90bf8767e24ba3c86da43c
JiyeonJeong01 <jizzvibe@gmail.com>
2025-09-01 20:50:18 +0900

    feat : add monster's dead logic

--- WinAPI-AirForce1945/CMonster.cpp
+++ WinAPI-AirForce1945/CMonster.cpp
@@ -1,6 +1,8 @@
 ﻿#include "pch.h"
 #include "CMonster.h"
 
+#include "CStageManager.h"
+
 CMonster::CMonster() : iHp(0), fPlayerVX(0.f), fPlayerVY(0.f), m_pBullet(nullptr), ull_wLastShotTime(0)
 {
 
@@ -16,11 +18,13 @@ bool CMonster::OnCollision(CObject* _pObjCol)
 	{
 	case OBJECT_TYPE::PLAYER:
 	{
+		CStageManager::Get_Instance()->On_MonsterKilled(this);
 		m_bDestroy = true;
 	}
 	break;
 	case OBJECT_TYPE::PLAYER_BULLET:
 	{
+ 		CStageManager::Get_Instance()->On_MonsterKilled(this);
 		m_bDestroy = true;
 	}
 	break;
