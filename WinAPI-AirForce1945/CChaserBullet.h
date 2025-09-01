7b3e1469ddb055aea1d54fa6dfce0368c90aa0ca
SoExDobin <56966606+SoexDobin@users.noreply.github.com>
2025-09-01 13:08:10 +0900

    chore : temp

--- WinAPI-AirForce1945/CChaserBullet.h
+++ WinAPI-AirForce1945/CChaserBullet.h
@@ -17,7 +17,9 @@ public:
 public:
 	void	Initialize() override;
 	int		Update() override;
+	void	LateUpdate() override;
 	void	Render(HDC _hDC) override;
+	void	Release() override;
 
 public:
 	bool	OnCollision(CObject* _pColObj) override;
